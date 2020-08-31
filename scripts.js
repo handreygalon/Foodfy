642a0ebfe9acf52939601f70e2d27fd72aa3349c
handreygalon <handrey.eg@gmail.com>
2020-08-31 08:32:18 -0300

    # Change modal image dynamically

--- scripts.js
+++ scripts.js
@@ -3,7 +3,9 @@ const recipes = document.querySelectorAll('.recipe')
 
 for (let recipe of recipes) {
     recipe.addEventListener('click', function() {
+        const img_name = recipe.getAttribute('id')
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector('img').src = `/assets/${img_name}.png`
     })
 }
 
