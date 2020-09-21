f3043b15a9b834f08f83d1eb34ab60b100ead2a8
handreygalon <handrey.eg@gmail.com>
2020-09-21 07:39:39 -0300

    # Added featured label in the recipes

--- data.js
+++ data.js
@@ -3,36 +3,42 @@ module.exports = [
         id: "burger",
         image: "/assets/burger.png",
         title: "Triplo bacon burger",
-        author: "Jorge Relato"
+        author: "Jorge Relato",
+        featured: true
     },
     {
         id: "pizza",
         image: "/assets/pizza.png",
         title: "Pizza 4 estações",
-        author: "Fabiana Melo"
+        author: "Fabiana Melo",
+        featured: false
     },
     {
         id: "espaguete",
         image: "/assets/espaguete.png",
         title: "Espaguete ao alho",
-        author: "Júlio Kinoto"
+        author: "Júlio Kinoto",
+        featured: true
     },
     {
         id: "lasanha",
         image: "/assets/lasanha.png",
         title: "Lasanha mac n' cheese",
-        author: "Juliano Vieira"
+        author: "Juliano Vieira",
+        featured: false
     },
     {
         id: "doce",
         image: "/assets/doce.png",
         title: "Docinho pão-do-céu",
-        author: "Ricardo Golvea"
+        author: "Ricardo Golvea",
+        featured: false
     },
     {
         id: "asinhas",
         image: "/assets/asinhas.png",
         title: "Asinhas de frango ao barbecu",
-        author: "Vania Steroski"
+        author: "Vania Steroski",
+        featured: true
     }
 ]
